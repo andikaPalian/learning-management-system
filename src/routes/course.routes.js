808bbca43c45ffa724c822cd0c6ea92623010e9f
Andika Palian <andika12des@gmail.com>
2025-02-27 09:29:00 +0800

    update course routes to allow ADMIN role for create, edit, and delete operations

--- src/routes/course.routes.js
+++ src/routes/course.routes.js
@@ -5,9 +5,9 @@ import { auth, hashRole } from '../middlewares/authMiddlewares.js';
 const courseRouter = express.Router();
 
 // Protected routes
-courseRouter.post("/create", auth, hashRole(['INSTRUCTOR']), createCourse);
-courseRouter.patch("/edit/:courseId", auth, hashRole(['INSTRUCTOR']), updateCourse);
-courseRouter.delete("/delete/:courseId", auth, hashRole(['INSTRUCTOR']), deleteCourse);
+courseRouter.post("/create", auth, hashRole(['INSTRUCTOR', 'ADMIN']), createCourse);
+courseRouter.patch("/edit/:courseId", auth, hashRole(['INSTRUCTOR', 'ADMIN']), updateCourse);
+courseRouter.delete("/delete/:courseId", auth, hashRole(['INSTRUCTOR', 'ADMIN']), deleteCourse);
 courseRouter.post("/join/:courseId", auth, hashRole(['STUDENT']), joinCourse);
 courseRouter.get("/list/students/:courseId", auth, hashRole(['INSTRUCTOR']), listStudents);
 
